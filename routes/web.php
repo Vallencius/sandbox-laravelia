4f58bafa07c4870a86f5f723a57fd81d7a59cf2b
Vallencius Gavriel <vallencius.siswanto@mobilepulsa.com>
2024-06-03 17:39:32 +0700

    Google Analytics

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use PostHog\PostHog;
+use Spatie\Analytics\Facades\Analytics;
+use Spatie\Analytics\Period;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,3 +16,18 @@ use PostHog\PostHog;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::domain('{subdomain}.' . env('APP_URL'))->group(function () {
+    Route::get('/', function () {
+        return view('welcome');
+    });
+    Route::get('/count', function () {
+        $visitorsAndPageViews       = Analytics::fetchVisitorsAndPageViews(Period::days(7));
+        $totalVisitorsAndPageViews  = Analytics::fetchTotalVisitorsAndPageViews(Period::days(7));
+        $mostVisitedPages           = Analytics::fetchMostVisitedPages(Period::days(7));
+        $topReferrers               = Analytics::fetchTopReferrers(Period::days(7));
+        $userTypes                  = Analytics::fetchUserTypes(Period::days(7));
+        $topBrowsers                = Analytics::fetchTopBrowsers(Period::days(7));
+
+        return view('count', compact('visitorsAndPageViews', 'totalVisitorsAndPageViews', 'mostVisitedPages', 'topReferrers', 'userTypes', 'topBrowsers'));
+    });
+});
